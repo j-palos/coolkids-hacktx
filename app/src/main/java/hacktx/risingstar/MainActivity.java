a1627ec0b06153924ab4f5e0302c45b288c6b5f6
OzoneUTLaptop <kafleyozone@utexas.edu>
2017-10-28 16:35:13 -0500

    initial commit

---
+++ app/src/main/java/hacktx/risingstar/MainActivity.java
@@ -0,0 +1,13 @@
+package hacktx.risingstar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
